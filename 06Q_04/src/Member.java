587cc2e7b598f9f3fda7be12c23b2a4f4beec56e
CBU <rvbear2000@gmail.com>
2022-11-04 02:48:46 +0900

    06Q_03에서 Member를 추상 클래스로 구현

---
+++ 06Q_04/src/Member.java
@@ -0,0 +1,44 @@
+abstract class Member {
+	private int Id;
+	private int hak;
+	private String name;
+
+	public Member(int Id, int hak, String name) {
+		this.hak = hak;
+		this.name = name;
+		this.Id = Id;
+	}
+
+	public void ShowInfo() {
+		System.out.print(Id + "\t" + hak + "\t" + name + "\t");
+	}
+	public abstract void Show();
+}
+
+class Mentor extends Member {
+	private String org;
+
+	public Mentor(int Id, int hak, String name, String org) {
+		super(Id, hak, name);
+		this.org = org;
+	}
+
+	public void Show() {
+		super.ShowInfo();
+		System.out.println(org);
+	}
+}
+
+class Mentee extends Member {
+	private String major;
+
+	public Mentee(int Id, int hak, String name, String major) {
+		super(Id, hak, name);
+		this.major = major;
+	}
+
+	public void Show() {
+		super.ShowInfo();
+		System.out.println(major);
+	}
+}
